d74ba072fa280119db6b9fff07861126bf967da2
naverlyn <rizalardiyantoro123@gmail.com>
2021-02-11 17:08:06 +0700

    add layout pembelian

---
+++ app/src/main/java/com/penjualan/nasigoreng/BeliLayout.java
@@ -0,0 +1,14 @@
+package com.penjualan.nasigoreng;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeliLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beli_layout);
+    }
+}

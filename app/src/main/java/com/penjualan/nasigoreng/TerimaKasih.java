27d7dc3fad24d0722beb3f228835ef1227ac6bad
naverlyn <rizalardiyantoro123@gmail.com>
2021-02-16 12:19:42 +0700

    masih blm diapa-apain

--- app/src/main/java/com/penjualan/nasigoreng/TerimaKasih.java
+++ app/src/main/java/com/penjualan/nasigoreng/TerimaKasih.java
@@ -2,18 +2,23 @@ package com.penjualan.nasigoreng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class TerimaKasih extends AppCompatActivity {
     String title = "Terima Kasih";
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terimakasih);
         setActionBar(this.title);
+        back = findViewById(R.id.back_to_homepage);
+        back.setOnClickListener(View -> balik());
     }
 
     private void setActionBar(String title)
@@ -23,4 +28,9 @@ public class TerimaKasih extends AppCompatActivity {
             getSupportActionBar().setTitle((CharSequence) title);
         }
     }
+    private void balik()
+        {
+            Intent b = new Intent(this, MainActivity.class);
+            startActivity(b);
+        }
 }
